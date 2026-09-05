2d88c8d34d50130560dea67098f248876d39f510
meg-zzz <143901779+meg-zzz@users.noreply.github.com>
2026-09-05 14:15:17 -0500

    Create Python dictionary from API with selected data

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
+#setup to retrieve API key from .env file
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
+import pandas as pd
+
+#Importing Materials Project API
 from mp_api.client import MPRester
 with MPRester(API_KEY) as mpr:
 
-    docs = mpr.materials.summary.search(
-        elements=["Fe", "Ni", "Cr"]
+#Selecting data to retrieve from Materials Project API in materials.summary
+    data = mpr.materials.summary.search(
+        fields=[
+            'material_id',
+            'formula_pretty',
+            'composition_reduced',
+            'density',
+            'bulk_modulus',
+            'shear_modulus',
+            'homogeneous_poisson',
+            'band_gap',
+            'is_stable',
+            'is_magnetic']
     )
-doc = docs[0]
-
-for result in doc:
-    print(result)
+#create python dictionary
+[mat.dict() for mat in data]
